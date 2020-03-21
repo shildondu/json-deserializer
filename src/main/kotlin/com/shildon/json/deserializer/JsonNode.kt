c27bdc1693df34e505b358ca5aa70726de4873c9
shildondu <shildondu@gmail.com>
2020-03-21 15:25:52 +0800

    add json node.

--- src/main/kotlin/com/shildon/json/deserializer/JsonNode.kt
+++ src/main/kotlin/com/shildon/json/deserializer/JsonNode.kt
@@ -1,13 +1,74 @@
 package com.shildon.json.deserializer
 
-data class JsonObject(
-        val field: Map<String, Any>
-)
+enum class JsonNodeType {
+    OBJECT,
+    ARRAY,
+    STRING,
+    NUMBER,
+    BOOLEAN,
+    NULL
+}
 
-data class JsonArray(
-        val field: List<Any>
-)
+interface TreeNode
 
-data class JsonValue(
-        val value: Any
-)
+abstract class JsonNode(
+        val type: JsonNodeType
+) : TreeNode {
+
+    fun asObjectNode() = this as ObjectNode
+
+    fun asArrayNode() = this as ArrayNode
+
+    fun asStringNode() = this as StringNode
+
+    fun asNumberNode() = this as NumberNode
+
+    fun asBooleanNode() = this as BooleanNode
+
+    fun asNullNode() = this as NullNode
+
+}
+
+class ObjectNode : JsonNode(JsonNodeType.OBJECT) {
+
+    val children = mutableMapOf<String, JsonNode>()
+
+    operator fun get(key: String) = children[key]
+
+}
+
+class ArrayNode : JsonNode(JsonNodeType.ARRAY) {
+
+    val items = mutableListOf<JsonNode>()
+
+    operator fun get(index: Int) = items[index]
+
+}
+
+class StringNode(
+        private val text: String
+) : JsonNode(JsonNodeType.STRING) {
+
+    fun value() = this.text
+
+}
+
+class NumberNode(
+        private val number: Number
+) : JsonNode(JsonNodeType.NUMBER) {
+
+    fun value() = this.number
+
+}
+
+class BooleanNode(
+        private val boolean: Boolean
+) : JsonNode(JsonNodeType.BOOLEAN) {
+
+    fun value() = this.boolean
+
+}
+
+class NullNode : JsonNode(JsonNodeType.NULL) {
+
+}
